37d9cdeb38f4207eaf10f80d38f93276f2b419ca
IshanP02 <2002ishan@gmail.com>
2025-08-03 13:38:40 -0400

    index implementation

---
+++ src/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const { Client, IntentsBitField, ActivityType } = require('discord.js');
+const eventHandler = require('./handlers/eventHandler');
+
+require('./database/db');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+eventHandler(client);
+
+//update token
+//client.login(process.env.TOKEN);
+
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is online.`);
+
+    client.user.setActivity({
+        name: 'FPL Season 1 Auction',
+        type: ActivityType.Watching,
+    });
+});
